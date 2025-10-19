51be7083accce0d4406cd26a42ffcc7fb299a93e
5horty <roganemacleod@gmail.com>
2025-10-19 17:20:18 +0100

    nice

--- rog/src/rogcat.rs
+++ rog/src/rogcat.rs
@@ -1,2 +1,11 @@
 use crate::init;
-pub struct cat {}
+use crate::repo_find::Repo;
+use std::io;
+
+pub fn cmd_cat_file(obj_type: &str, obj_name: &str) -> io::Result<()> {
+    let repo = Repo::find_file(None)?;
+}
+
+pub fn cat_file(repo: &Repo, obj_name: &str, fmt: Option<&str>) -> io::Result<()> {
+    let obj_id = 
+}
